bc32e6663c7daeace54f1969a222a10ec1553956
smaylninja <smaylninja@gmail.com>
2025-04-10 22:42:18 +0500

    feat: add create and edit routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import HomeView from "@/views/HomeView.vue";
+import TaskCreateView from "@/views/TaskCreateView.vue";
+import TaskEditView from "@/views/TaskEditView.vue";
+import { useTaskStore } from "@/stores/task";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +11,16 @@ const router = createRouter({
       path: "/",
       name: "home",
       component: HomeView,
+    },
+    {
+      path: "/create",
+      name: "task-create",
+      component: TaskCreateView,
+    },
+    {
+      path: "/edit/:id",
+      name: "task-edit",
+      component: TaskEditView,
     },
   ],
 });
