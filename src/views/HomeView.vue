826e8362e4d6854c7e17f51eac44658a7a574d7f
smaylninja <smaylninja@gmail.com>
2025-04-10 22:41:42 +0500

    feat: add tasks table, tasks counter and footer

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,49 @@
-<script setup></script>
+<script setup lang="ts">
+import TasksCounter from "@/components/TasksCounter.vue";
+import TasksTable from "@/components/TasksTable.vue";
+import VButton from "@/components/UI/VButton.vue";
+import { useTaskStore } from "@/stores/task";
 
-<template>Home</template>
+const taskStore = useTaskStore();
+
+const deleteTask = (id: number) => {
+  taskStore.deleteTask(id);
+};
+</script>
+
+<template>
+  <div class="home">
+    <TasksTable
+      :tasks="taskStore.sortedTasksByBugs"
+      @delete-task="deleteTask"
+    />
+
+    <footer>
+      <TasksCounter
+        :tasks-count="taskStore.tasks.length"
+        :bugs-count="taskStore.bugsCount"
+      />
+      <RouterLink to="/create">
+        <VButton variant="accent" prepend-icon="plus">
+          Добавить задачу
+        </VButton>
+      </RouterLink>
+    </footer>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.home {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 100%;
+
+  & > footer {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 1em;
+  }
+}
+</style>
