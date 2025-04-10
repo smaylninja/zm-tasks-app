851e57a6da2b63de68433ba8af6a4aecd365111a
smaylninja <smaylninja@gmail.com>
2025-04-10 22:18:42 +0500

    feat: add logo component

---
+++ src/components/TheLogo.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup></script>
+
+<template>
+  <header class="app-header">
+    <img src="../assets/logo.svg" />
+    <h1 class="app-name">Задачник</h1>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+.app-header {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  & > .app-name {
+    font-weight: 600;
+    font-size: 20px;
+    line-height: 100%;
+    letter-spacing: 0%;
+    text-align: center;
+    color: var(--logo-title);
+    margin-top: 5px;
+  }
+}
+</style>
