4c5748b79728f8aa4955495b2199dff25eacda58
smaylninja <smaylninja@gmail.com>
2025-04-10 22:43:11 +0500

    feat: add route guard to prevent getting task by invalid id

--- src/router/index.js
+++ src/router/index.js
@@ -21,6 +21,15 @@ const router = createRouter({
       path: "/edit/:id",
       name: "task-edit",
       component: TaskEditView,
+      beforeEnter: (to) => {
+        const taskStore = useTaskStore();
+
+        if (taskStore.getTaskById(+to.params.id)) {
+          return true;
+        } else {
+          router.push("/");
+        }
+      },
     },
   ],
 });
