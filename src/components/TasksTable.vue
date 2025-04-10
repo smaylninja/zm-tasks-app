e3f219cf4ada43e3aacb0977a8eb32325905ddee
smaylninja <smaylninja@gmail.com>
2025-04-10 22:19:19 +0500

    feat: add tasks table component

---
+++ src/components/TasksTable.vue
@@ -0,0 +1,85 @@
+<script lang="ts" setup>
+import { Task } from "@/models/task.model";
+
+defineProps<{
+  tasks: Task[];
+}>();
+
+const emit = defineEmits(["deleteTask"]);
+</script>
+
+<template>
+  <div class="tasks">
+    <template v-for="task in tasks" :key="task.id">
+      <div class="task-item" :class="{ bug: task.isBug }">
+        <div class="task-name">
+          <span class="task-id">{{ `# ${task.id}` }}</span>
+          &nbsp;
+          <p>{{ task.name }}</p>
+        </div>
+
+        <div class="buttons">
+          <RouterLink :to="`/edit/${task.id}`">
+            <img class="button" src="../assets/icons/edit.svg" />
+          </RouterLink>
+
+          <img
+            class="button"
+            src="../assets/icons/delete.svg"
+            @click="emit('deleteTask', task.id)"
+          />
+        </div>
+      </div>
+    </template>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.tasks {
+  display: flex;
+  flex-direction: column;
+
+  & > .task-item {
+    padding: 8px 19px;
+    border-bottom: 1px solid #cdcdcd;
+    display: flex;
+    justify-content: space-between;
+    align-items: start;
+    min-width: 356px;
+    color: var(--text-primary);
+
+    & > .task-name {
+      display: flex;
+    }
+
+    & > .task-name > .task-id {
+      font-weight: 700;
+      color: var(--text-primary-pale);
+      white-space: nowrap;
+    }
+
+    &:last-child {
+      border-bottom: none;
+    }
+
+    & > .buttons {
+      display: flex;
+      gap: 4px;
+      margin-left: 4px;
+
+      & > .button {
+        &:hover {
+          cursor: pointer;
+        }
+      }
+    }
+  }
+
+  & > .bug {
+    color: var(--text-bug);
+    & > .task-name > .task-id {
+      color: var(--text-bug-pale);
+    }
+  }
+}
+</style>
