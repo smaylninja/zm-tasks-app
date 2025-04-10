4d4084a99944d91633832cae0c95e89e5c7c9412
smaylninja <smaylninja@gmail.com>
2025-04-10 22:43:32 +0500

    feat: add main layout

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,32 @@
-<script setup lang="ts">
-import { RouterView } from "vue-router";
+<script lang="ts" setup>
+import TheLogo from "@/components/TheLogo.vue";
 </script>
 
 <template>
-  <RouterView />
+  <div class="container">
+    <TheLogo class="logo" />
+
+    <section class="view-content">
+      <RouterView />
+    </section>
+  </div>
 </template>
 
-<style scoped></style>
+<style lang="scss" scoped>
+.container {
+  height: 100vh;
+  padding: 57px 0 67px 0;
+  display: grid;
+  grid-template-rows: auto 1fr;
+
+  & > .logo {
+    margin-bottom: 66px;
+  }
+
+  & > .view-content {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+}
+</style>
