f797cc81bd4c5fdf005e0feca574decc43197aa5
smaylninja <smaylninja@gmail.com>
2025-04-10 22:19:01 +0500

    feat: add task counter component

---
+++ src/components/TasksCounter.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+const { tasksCount = 0, bugsCount = 0 } = defineProps<{
+  tasksCount: number;
+  bugsCount: number;
+}>();
+</script>
+
+<template>
+  <div class="task-counter">
+    <span>Задач: {{ tasksCount }} / Багов: {{ bugsCount }}</span>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.task-counter {
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 130%;
+  letter-spacing: -1%;
+  color: var(--text-primary);
+}
+</style>
